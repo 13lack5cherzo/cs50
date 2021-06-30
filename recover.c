15cb12cf58fdb5f008440099ad391cb6a8bd3a8d
13lack5cherzo <13lack5cherzo@users.noreply.github.com>
2021-06-30 12:18:03 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -1,90 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+typedef uint8_t BYTE;
+
 
 int main(int argc, char *argv[])
 {
-    // Check usage
     if (argc != 2)
     {
-        return 1;
+    printf("Usage: ./recover image\n");
+    return 1;
     }
 
-    // Open file
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
+    else
     {
-        printf("Could not open file.\n");
-        return 1;
-    }
+        // Open card.raw
+        char *input_file_name = argv[1];
+        FILE *input_pointer = fopen(input_file_name, "r");
 
-
-    int file_num = 0; // file number
-    FILE *output = NULL; // output pointer
-    char file_name[7]; // filename
-    uint8_t chunk[512]; // 512 data chunk
-    int chunk_num = 0; // chunk number
-
-    while(
-        fread(&chunk, sizeof(chunk), 1, file) == 1
-        )
+        if (input_pointer == NULL)
         {
-            // printf("read chunk %i\n", chunk_num);
-            // chunk_num += 1;
-
-            // Check first 4 bytes
-            if (
-                (chunk[0] == 0xff)
-                && (chunk[1] == 0xd8)
-                && (chunk[2] == 0xff)
-                && ((chunk[3] & 0xf0) == 0xe0)
-                )
-            {
-                // //
-                // if after the first file, close file
-                // //
-                if (file_num > 0)
-                {
-                    fclose(output);
-                }
-
-                // //
-                // generate file name
-                // //
-                sprintf(file_name, "%03i.jpg", file_num);
-                printf("%s\n", file_name); // debug
-
-
-                // //
-                // open file to write to
-                // //
-                output = fopen(file_name, "w");
-                if (output == NULL)
-                {
-                    printf("Could not open file.\n");
-                    return 1;
-                }
-
-                // add one to file number counter
-                file_num = file_num + 1;
-
-            // //
-            // write to output file
-            // //
-
-            if (file_num > 0)
-            {
-                fwrite(&chunk, sizeof(chunk), 1, output);
-            }
-
-
-            }
+            printf("Error: cannot open %s\n", input_file_name);
+            return 2;
         }
 
-    // Close file
-    fclose(file);
-    fclose(output);
+
+        // Initialise variables
+        BYTE buffer[512];
+        int count = 0;
+        FILE *img_pointer = NULL;
+        char filename[8];
+
+        // Repeat until end of card:
+        while (fread(&buffer, 512, 1, input_pointer) == 1)
+        {
+            // If start of a new JPEG (0xff 0xd8 0xff 0xe*):
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+            {
+                // If not first JPEG, close previous
+                if (!(count == 0))
+                {
+                    fclose(img_pointer);
+                }
+                // Initialise file
+                sprintf(filename, "%03i.jpg", count);
+                img_pointer = fopen(filename, "w");
+                count++;
+            }
+            // If JPEG has been found, write to file
+            if (!(count == 0))
+            {
+                fwrite(&buffer, 512, 1, img_pointer);
+            }
+        }
+        fclose(input_pointer);
+        fclose(img_pointer);
+        return 0;
+    }
 }
-
-
-

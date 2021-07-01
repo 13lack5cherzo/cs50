a6b23e511fa6ac554ed3c32e675cfe85de5e8513
13lack5cherzo <13lack5cherzo@users.noreply.github.com>
2021-07-01 08:45:39 +0000

    automated commit by submit50

--- dictionary.c
+++ dictionary.c
@@ -1,137 +1,149 @@
+// Implements a dictionary's functionality
+
 #include <stdbool.h>
-#include "dictionary.h"
 #include <stdio.h>
-#include <cs50.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
-#include <strings.h>
-//represents a node in a hash table
+#include <ctype.h>
+#include "dictionary.h"
+
+// Represents a node in a hash table
 typedef struct node
 {
     char word[LENGTH + 1];
     struct node *next;
 }
 node;
-// no. of buckets in hash table
-const unsigned int N = 26;
-// initialise positive hash value using unsigned int
-unsigned int hash_value;
-// initialise (positive) hash table word count
-unsigned int word_count;
-// hash table
+
+// Number of buckets in hash table
+const unsigned int N = 26 * 26 + 1;
+
+// Number of words in dictionary
+unsigned int w_count = 0;
+
+// Hash table
 node *table[N];
 
-// Loads dict into memory, returning true if successful else false
-bool load(const char *dictionary)
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
 {
-    // Open dict
-    FILE *file = fopen(dictionary, "r");
-// If file is not opened, return false
-    if (file == NULL)
+    // hash word
+    unsigned int hidx = hash(word);
+
+    // traverse nodes in table[hidx] to find match
+    for (node *tempnode = table[hidx]; tempnode != NULL; tempnode = tempnode->next)
     {
-        return false;
-    }
-    // storage space for word
-    char word[LENGTH + 1];
-// Scan dict for strings that are not the end of the file
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        // Allocate memory for new node
-        node *n = malloc(sizeof(node));
-// If malloc returns NULL, return false
-        if (n == NULL)
+        // if match is found,
+        if (tempnode->word == word)
         {
-            return false;
+            return true;
         }
-// Pointer to next node and word itself
-        strcpy(n->word, word);
-        // Hash the word to get hash value
-        hash_value = hash(word);
-        // Set new pointer
-        n->next = table[hash_value];
-        // Set head to new pointer
-        table[hash_value] = n;
-        // Increment word count
-        word_count++;
     }
-// Close file
-    fclose(file);
-// If dict is loaded, return true
-    return true;
+    // if no match is found,
+    return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    unsigned long hash = 5381;
-    int c;
-    while ((c = toupper(*word++)))
+    if (isalpha(word[0]) == 0)
     {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        // not alphabet
+        return (N - 1);
     }
-    return hash % N;
+    else
+    {
+        // get alphabet index of first and second letters
+        // 'a' starts from 1
+        int l1 = tolower(word[0]) - 'a' + 1;
+        int l2 = tolower(word[1]) - 'a' + 1;
+
+        // handle edge cases
+        if ((l1 < 1) || (l1 > 26))
+        {
+            l1 = 26; // put in last bucket
+        }
+        if ((l2 < 1) || (l2 > 26))
+        {
+            l2 = 26; // put in last bucket
+        }
+
+        return (l1 * l2 - 1);
+    }
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // open dictionary file
+    FILE *dic_pointer = fopen(dictionary, "r");
+    if (dic_pointer == NULL)
+    {
+        printf("Error: cannot open %s\n", dictionary);
+        return 2;
+    }
+
+    // Set hash table all to null
+    for (int tidx = 0; tidx < N; tidx++)
+    {
+        table[tidx]->next = NULL;
+    }
+
+    // read strings from file one at a time
+    char word[LENGTH + 1]; // placeholder
+    int hidx = 0; // hashed word index
+    while(fscanf(dic_pointer, "%s", word) != EOF)
+    {
+        // allocate memory for node
+        node *n = malloc(sizeof(node));
+        if (n == NULL) // exit if not read
+        {
+            return false;
+        }
+
+        // copy word into node
+        strcpy(n->word, word);
+
+        // hash word
+        hidx = hash(word);
+
+        // insert node into hash table
+        n->next = table[hidx];
+        table[hidx] = n;
+
+        // keep track of number of words in dictionary
+        w_count++;
+    }
+
+    fclose(dic_pointer);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // Check if there are any words
-    if (word_count > 0)
-    {
-        // Return count
-        return word_count;
-    }
-    // Else
-    return 0;
+    return w_count;
 }
 
-// Returns true if word is in dictionary else false
-bool check(const char *word)
-{
-    //hash the word to get hash value
-    hash_value = hash(word);
-    //access the linked list
-    node *cursor = table[hash_value];
-//go through the linked list
-    while (cursor != NULL)
-    {
-        //check if the word matches
-        if (strcasecmp(word, cursor->word) == 0)
-        {
-            return true;
-        }
-//move cursor to next node
-        cursor = cursor->next;
-    }
-return false;
-}
-
-
-
-// Unloads dictionary from memory, returning true if successful else false
+// Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    // Iterate through buckets
-    for (int i = 0; i < N; i++)
-    {
-        // Set cursor to this each bucket location
-        node *cursor = table[i];
-// If cursor is not NULL, free
-        while (cursor)
-        {
-            // Create temp
-            node *tmp = cursor;
-            // Move cursor to next node
-            cursor = cursor->next;
-            // Free up temp
-            free(tmp);
-        }
-// If cursor is NULL
-        if (i == N - 1 && cursor == NULL)
-        {
-            return true;
-        }
-    }
+    // TODO
     return false;
+}
+
+// free linked list
+void free_ll(node *nf)
+{
+    // Handle base case
+    if (nf == NULL)
+    {
+        return;
+    }
+    // linked nodes
+    free_ll(nf->next);
+    // free node
+    free(nf);
 }

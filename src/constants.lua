e41032e976cbd7947ecaf56fd81ac1e005c921ea
13lack5cherzo <chan_dan1995@hotmail.com>
2021-09-05 14:26:34 +0800

    initial_commit

---
+++ src/constants.lua
@@ -0,0 +1,23 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    -- constants --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+VIRTUAL_WIDTH = 384
+VIRTUAL_HEIGHT = 216
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+TILE_SIZE = 16
+
+OPPONENT_LEVEL_SCALING = 2
+
+DEBUG_MODE = false
+
+

cd33608276ecf2ac5011c88f8f8329afeab4dc61
13lack5cherzo <chan_dan1995@hotmail.com>
2021-09-11 15:18:33 +0800

    initial_commit

---
+++ Assets/FinishGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Text))]
+public class FinishGame : MonoBehaviour
+{
+
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("CompleteText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Finish")
+        {
+            text.text = "LEVEL COMPLETE";
+        }
+    }
+
+}
